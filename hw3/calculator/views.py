4ffb6e7c88d9487c2e4a2bee53382314e6d7ac71
Di Lu <dilu@andrew.cmu.edu>
2021-02-17 00:09:52 -0500

    try to realize real-time single num show; succeed in simple url maintainance

--- hw3/calculator/views.py
+++ hw3/calculator/views.py
@@ -2,18 +2,65 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
+
 # Create your views here.
+def plus(a, b):
+    return int(a) + int(b)
+
+
+def minus(a, b):
+    return int(a) - int(b)
+
+
+def multiple(a, b):
+    return int(a) * int(b)
+
+
+def divide(a, b):
+    return int(int(a) / int(b))
+
 
 def home_page(request):
-    # render takes: (1) the request,
-    #               (2) the name of the view to generate, and
-    #               (3) a dictionary of name-value pairs of data to be
-    #                   available to the view.
-    return render(request, 'calculator/calculator.html', {})
+    context = {}
+    context['prev_opr'] = '+'
+    context['prev_val'] = '0'
+    context['new_val'] = '0'
+    context['cal_result'] = '0'
+    return render(request, 'calculator/calculator.html', context)
+
+def do_calculation(request):
+    context = {}
+    if 'opr' in request.GET:
+        if request.GET['opr'] == '+':
+            prev_val = request.GET['calc_result']
+            new_val = '0'
+            context['prev_opr'] = '&/plus;'
+            context['prev_val'] = request.GET['calc_result']
+            context['new_val'] = '0'
+            context['calc_result'] = plus(prev_val,new_val)
+            return render(request,'calculator/calculator.html',context)
+    return render(request, 'calculator/calculator.html', context)
+
+def setNum(request):
+    # if 'num' in request.POST:
+    new_val = request.POST['button']
+    context = {}
+    context['prev_opr'] = '+'
+    context['prev_val'] = '0'
+    context['new_val'] = '0'
+    context['cal_result'] = new_val
+    return render(request, 'calculator/calculator.html', context)
 
 def calculator(request):
-    # render takes: (1) the request,
-    #               (2) the name of the view to generate, and
-    #               (3) a dictionary of name-value pairs of data to be
-    #                   available to the view.
-    return render(request, 'templates/calculator/calculator.html', {})
+    context = {}
+    if 'button' in request.POST:
+        new_val = request.POST['button']
+        cal_result = '0'
+        prev_opr = '+'
+
+        context = {}
+        context['prev_opr'] = '+'
+        context['prev_val'] = '0'
+        context['new_val'] = new_val
+        context['cal_result'] = new_val
+        return render(request, 'calculator/calculator.html', context)
