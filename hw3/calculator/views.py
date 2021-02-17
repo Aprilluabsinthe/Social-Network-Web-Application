4f3d8627f8eff28140756f83b9acb720a028e63a
Di Lu <dilu@andrew.cmu.edu>
2021-02-17 02:46:04 -0500

    display single number in display area

--- hw3/calculator/views.py
+++ hw3/calculator/views.py
@@ -28,39 +28,36 @@ def home_page(request):
     context['cal_result'] = '0'
     return render(request, 'calculator/calculator.html', context)
 
-def do_calculation(request):
-    context = {}
-    if 'opr' in request.GET:
-        if request.GET['opr'] == '+':
-            prev_val = request.GET['calc_result']
-            new_val = '0'
-            context['prev_opr'] = '&/plus;'
-            context['prev_val'] = request.GET['calc_result']
-            context['new_val'] = '0'
-            context['calc_result'] = plus(prev_val,new_val)
-            return render(request,'calculator/calculator.html',context)
-    return render(request, 'calculator/calculator.html', context)
+# def do_calculation(request):
+#     context = {}
+#     if 'opr' in request.GET:
+#         if request.GET['opr'] == '+':
+#             prev_val = request.GET['calc_result']
+#             new_val = '0'
+#             context['prev_opr'] = '&/plus;'
+#             context['prev_val'] = request.GET['calc_result']
+#             context['new_val'] = '0'
+#             context['calc_result'] = plus(prev_val,new_val)
+#             return render(request,'calculator/calculator.html',context)
+#     return render(request, 'calculator/calculator.html', context)
 
-def setNum(request):
-    # if 'num' in request.POST:
+# def setNum(request):
+#     # if 'num' in request.POST:
+#     new_val = request.POST['button']
+#     context = {}
+#     context['prev_opr'] = '+'
+#     context['prev_val'] = '0'
+#     context['new_val'] = '0'
+#     context['cal_result'] = new_val
+#     return render(request, 'calculator/calculator.html', context)
+
+def calculator(request):
+    context = {}
+    # if 'button' in request.session:
     new_val = request.POST['button']
     context = {}
     context['prev_opr'] = '+'
     context['prev_val'] = '0'
-    context['new_val'] = '0'
+    context['new_val'] = new_val
     context['cal_result'] = new_val
     return render(request, 'calculator/calculator.html', context)
-
-def calculator(request):
-    context = {}
-    if 'button' in request.POST:
-        new_val = request.POST['button']
-        cal_result = '0'
-        prev_opr = '+'
-
-        context = {}
-        context['prev_opr'] = '+'
-        context['prev_val'] = '0'
-        context['new_val'] = new_val
-        context['cal_result'] = new_val
-        return render(request, 'calculator/calculator.html', context)
