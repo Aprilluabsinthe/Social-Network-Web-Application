576c6f16fda2bfbd9fcbad0366c90a865904d957
Di Lu <dilu@andrew.cmu.edu>
2021-02-16 19:01:26 -0500

    create Django project 'calculator'

--- hw3/README.md
+++ hw3/README.md
@@ -1,1 +1,11 @@
-Please put hw3 files in this directory.
+1) Andrew ID: dilu
+2) Name: Di Lu
+3) Homework: #3
+4) Andrew IDs of students that I consulted on this homework: None
+5) Andrew IDs of course staff that I consulted on this homework: None
+6) Names of other people I consulted on this homework: None
+7) List of external sources I consulted when solving this homework:
+8) Integrity Statement: I, Di Lu, did not electronically copy any
+source code, in whole or in part, from any source, other than the course
+examples provided to me this semester, when preparing my solution for this
+homework assignment.    
---
+++ hw3/calculator/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+# Create your views here.
+
+def home_page(request):
+    # render takes: (1) the request,
+    #               (2) the name of the view to generate, and
+    #               (3) a dictionary of name-value pairs of data to be
+    #                   available to the view.
+    return render(request, 'calculator/calculator.html', {})
+
+def calculator(request):
+    # render takes: (1) the request,
+    #               (2) the name of the view to generate, and
+    #               (3) a dictionary of name-value pairs of data to be
+    #                   available to the view.
+    return render(request, 'templates/calculator/calculator.html', {})
