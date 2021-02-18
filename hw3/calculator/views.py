d19f134438d8daa8129f7f978df4c8c86138623d
Di Lu <dilu@andrew.cmu.edu>
2021-02-18 12:49:57 -0500

    delete session, but failed in url showing and previous maintenance

--- hw3/calculator/views.py
+++ hw3/calculator/views.py
@@ -23,11 +23,7 @@ def update(a,b):
     return int(float(a)) * 10 + int(b)
 
 def home_page(request):
-    request.session.set_test_cookie()
     context = {}
-    request.session['prev_opr'] = "plus"
-    request.session['prev_val'] = '0'
-    request.session['new_val'] = '0'
     context['prev_opr'] = "plus"
     context['prev_val'] = '0'
     context['new_val'] = '0'
@@ -37,44 +33,29 @@ def home_page(request):
 
 def calculator(request):
     errors = []
-    if 'prev_opr' not in request.session:
-        errors.append("Error: Must Have Previous Operator")
-    if 'prev_val' not in request.session:
-        errors.append("Error: Must Have Previous Value")
-    if 'new_val' not in request.session:
-        errors.append("Error: Must Have New Value")
-    if errors:
-        return render(request, 'calculator/calculator.html', {'errors': errors})
 
     context = {}
     try:
-        prev_opr = request.session['prev_opr']
-        prev_val = request.session['prev_val']
-        new_val = request.session['new_val']
+        prev_opr = request.POST['prev_opr']
+        prev_val = request.POST['prev_val']
+        new_val = request.POST['new_val']
+        buttonvalue = request.GET['button']
     except:
-        errors.append("Session Error: Session be changed")
-        return render(request, 'calculator/calculator.html', {'errors': errors})
+        if 'prev_opr' not in request.POST or request.POST['prev_opr'] not in ['plus', 'minus', 'times', 'divide']:
+            errors.append("Operation Error: Previous Operator changed or deleted")
+        if 'prev_val' not in request.POST or not request.POST['prev_val'].isnumeric():
+            errors.append("ValueError: Previous Value changed or deleted")
+        if 'new_val' not in request.POST or not request.POST['new_val'].isnumeric():
+            errors.append("ValueError: New Value changed or deleted")
+        if 'button' not in request.POST:
+            errors.append("ValueError: no button clicked")
 
-    if 'button' not in request.GET:
-        errors.append("ValueError: no button clicked")
-    if 'prev_opr' not in request.GET or request.GET['prev_opr'] not in ['plus', 'minus', 'times', 'divide']:
-        errors.append("Operation Error: Previous Operator changed or deleted")
-    if 'prev_val' not in request.GET or not request.GET['prev_val'].isnumeric():
-        errors.append("ValueError: Previous Value changed or deleted")
-    if 'new_val' not in request.GET or not request.GET['new_val'].isnumeric():
-        errors.append("ValueError: New Value changed or deleted")
     if errors:
         return render(request, 'calculator/calculator.html', {'errors': errors})
 
     else:
-        try:
-            buttonvalue = request.GET['button']
-        except:
-            errors.append("ValueError: no button clicked")
-            return render(request, 'calculator/calculator.html', {'errors': errors})
         if buttonvalue in [chr(x) for x in range(48,58)]:
             update_val = str(update(new_val,buttonvalue))
-            request.session['new_val'] = update_val
             context['prev_opr'] = prev_opr
             context['prev_val'] = prev_val
             context['new_val'] = update_val
@@ -96,20 +77,20 @@ def calculator(request):
                     return render(request, 'calculator/calculator.html', {'errors': errors})
 
                 if buttonvalue in ['plus', 'minus', 'times', 'divide']:
-                    request.session['prev_opr'] = buttonvalue
-                    request.session['prev_val'] = str(cal_result)
-                    request.session['new_val'] = '0'
+                    prev_opr = buttonvalue
+                    prev_val = str(cal_result)
+                    new_val = '0'
                 elif buttonvalue == 'equals':
-                    request.session['prev_opr'] = 'plus'
-                    request.session['prev_val'] = '0'
-                    request.session['new_val'] = '0'
+                    prev_opr = 'plus'
+                    prev_val = '0'
+                    new_val = '0'
                 else:
                     errors.append("Button invalid: button value changed")
                     return render(request, 'calculator/calculator.html', {'errors': errors})
-                # renew sessions
-                context['prev_opr'] = request.session['prev_opr']
-                context['prev_val'] = request.session['prev_val']
-                context['new_val'] =  request.session['new_val']
+                # renew memories
+                context['prev_opr'] = prev_opr
+                context['prev_val'] = prev_val
+                context['new_val'] =  new_val
                 context['cal_result'] = str(cal_result)
                 context['errors'] = errors
                 return render(request, 'calculator/calculator.html', context)
