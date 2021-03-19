0d68e4696d59c26bd9d196c6448063200a04c01c
Di Lu <dilu@andrew.cmu.edu>
2021-03-19 13:24:23 -0400

    change static_root and database setting

--- hw7/webapps/settings.py
+++ hw7/webapps/settings.py
@@ -86,8 +86,10 @@ WSGI_APPLICATION = 'webapps.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'socialnetwork',
+        'USER': '',
+        'PASSWORD': '',
     }
 }
 
@@ -129,3 +131,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
